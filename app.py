de1b11bf5168a06fb6ade9fb2d8e2f56da1a35a2
han120cm <alfalih.hanif@gmail.com>
2025-07-18 21:08:45 +0700

    change port to 8080

--- app.py
+++ app.py
@@ -269,4 +269,4 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     start_cdn_healthcheck()
-    app.run(debug=True, host="0.0.0.0", port=8000)
+    app.run(debug=True, host="0.0.0.0", port=8080)
